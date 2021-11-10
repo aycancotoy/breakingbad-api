01b417c4a5a059dc8bd93403c5c3ab79347a1328
aycan <aycancotoy@gmail.com>
2021-11-10 16:41:37 +0300

    Querry search kısmı yapıldı.

--- src/App.js
+++ src/App.js
@@ -3,27 +3,32 @@ import axios from "axios";
 import "./App.css";
 import Header from "./components/ui/Header";
 import CharacterGrid from "./components/characters/CharacterGrid";
+import Search from "./components/ui/Search";
 
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState(true);
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://breakingbadapi.com/api/characters`);
+      const result = await axios(
+        `https://breakingbadapi.com/api/characters?name=${query}`
+      );
 
-      console.log(result.data);
+      // console.log(result.data);
 
       setItems(result.data);
       setIsLoading(false);
     };
 
     fetchItems();
-  }, []);
+  }, [query]);
 
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(q) => setQuery} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
