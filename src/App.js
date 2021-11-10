269a02699a5bfbbadca13ceaa9bda6eceadf7837
aycan <aycancotoy@gmail.com>
2021-11-10 15:57:26 +0300

    App js'de filtreleme yapıldı, Yeni component oluşturuldu.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
+import Header from "./components/ui/Header";
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://breakingbadapi.com//api/characters`);
+
+      console.log(result.data);
+
+      setItems(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, []);
+
   return (
     <div className="container">
-      <h1>TEST</h1>
+      <Header />
     </div>
   );
 };
